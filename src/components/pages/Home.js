a0c1c04d19a02c12ef3ad550e3eefc138a30c7c9
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-04-24 00:18:50 +0100

    ✨ feat(F01): Home > Ajout du composant Category dans <Home/>

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import Category from './sections/Category'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+        <Category/>
+    </div>
   )
 }
