34ad23c2733136460e3367570f6309b1850fd8df
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-04-24 00:54:34 +0100

    ➕ feat(F01): intégration de slugify et affichage des elements de Category

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
+//import './App.css';
 import Home from './components/pages/Home';
+import Category from './components/pages/sections/Category';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home/>} />
+        <Route path='/' exact element={<Home/>} />
+        <Route path='/category' element={<Category/>} />
       </Routes>
     </>
   );
