e6d62d651950dcfe63e94ee429511f7d83b4f449
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-04-24 02:04:12 +0100

    ✨ feat(F01): ajout des composants Navbar et Footer dans <App/>

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import { Route, Routes } from 'react-router-dom';
 //import './App.css';
 import Home from './components/pages/Home';
 import Category from './components/pages/sections/Category';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
+      <Navbar/>
       <Routes>
-        <Route path='/' exact element={<Home/>} />
+        <Route path='/' element={<Home/>} />
         <Route path='/category' element={<Category/>} />
       </Routes>
+      <Footer/>
     </>
   );
 }
