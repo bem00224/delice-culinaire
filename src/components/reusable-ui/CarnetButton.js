5549970f330edae8204c77394fe7801a376621f3
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-04-24 19:05:37 +0100

    ♻️ refactor(F02) : amélioration du composant CarnetButton avec ajout du button

--- src/components/reusable-ui/CarnetButton.js
+++ src/components/reusable-ui/CarnetButton.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { CiHeart } from 'react-icons/ci'
+import '../../styles/carnetButton.css'
 
 export default function CarnetButton() {
+  const [ isSaved, setIsSaved] = useState(false)
   return (
-    <div>CarnetButton</div>
+    <div className='carnet-details-btn-content'>
+        <button className='carnet-details-btn'>
+            <CiHeart className='icon-heart'/> 
+            { isSaved ? "Sauvegardée":"Mon carnet"}
+        </button>
+    </div>
   )
 }
