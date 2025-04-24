7cf7535316063c03c4779851f319d587e96af50c
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-04-24 19:04:05 +0100

    ♻️ refactor(F02) : amélioration du composant Title avec ajout du titre dynamique et du retour

--- src/components/reusable-ui/Image.js
+++ src/components/reusable-ui/Image.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import { IoMdTime } from 'react-icons/io'
+import { PiCookingPot } from 'react-icons/pi'
+import '../../styles/image.css'
 
-export default function Image() {
+export default function Image({ imageSource, title, preparationTime, cookingTime }) {
   return (
-    <div>Image</div>
+    <div className='category-details-recipe-bottom'>
+        <div className='category-details-recipe-bottom-image'>
+            <img src={imageSource} alt={title} className="details-image" />
+        </div>
+        <div className="details-info">
+            <p className='details-info-global'>
+                <IoMdTime className='details-info-icon' />
+                <span className='details-info-text'> Préparation : {preparationTime} min </span>
+            </p>
+            <p className='details-info-global'>
+                <PiCookingPot className='details-info-icon'/> 
+                <span className='details-info-text'>Cuisson : {cookingTime} min </span>
+            </p>
+        </div>
+    </div>
   )
 }
