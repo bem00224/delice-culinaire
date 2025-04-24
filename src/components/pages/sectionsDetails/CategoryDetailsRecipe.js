7b4eb1c6e4ba746b87ea7984666403ef23928ea0
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-04-24 14:00:02 +0100

    ✨ feat(F02): 🎉 affichage des détails de recette via slug

--- src/components/pages/sectionsDetails/CategoryDetailsRecipe.js
+++ src/components/pages/sectionsDetails/CategoryDetailsRecipe.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { fakeData } from '../../../data/fakeData'
 
 export default function CategoryDetailsRecipe() {
+    const { slug } = useParams()
+    //on cherche d'abord la catégory qui contient cette recette
+    const category = fakeData.Category.find((cat) => cat.recettes.some(rec => rec.slug === slug))
+
+    if(!category) {
+        return <div>Recette non trouvée 🥲</div>
+    }
+    
+
+    const recipe = category.recettes.find((rec) => rec.slug === slug)
+    console.log('recipe', recipe);
+    if(!recipe) {
+        return <div>Détails de la recette introuvables ❌</div>
+    }
+    
+    
   return (
-    <div>CategoryDetailsRecipe</div>
+    <div className="currentnews-details">
+        <div>
+            <div className='content-titleBar'>
+                <h2 className="details-title">{recipe.title}</h2>
+            </div>
+        </div>
+    </div>
   )
 }
