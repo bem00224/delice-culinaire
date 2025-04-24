340f858748c3d429fcef58bf5eca539ce32b1f88
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-04-25 00:03:50 +0100

    ✨  feat(F03):  Ajout d'une Navbar responsive avec menu burger et navigation mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/navbar.css'
+import { FaBars, FaTimes } from "react-icons/fa";
+import logo from "../assets/logo.png"
+
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
-    <div>Navbar</div>
+    <header className="navbar">
+      <div className='navbar-container'>
+        <Link to="/" className="navbar-logo">
+          <img src={logo} alt="logo" className="logo" />
+        </Link>
+        <nav className={`navbar-menu ${isOpen ? "open" : ""}`}>
+          <Link to="/mes-recettes" className="btn desktop-only">Mes recettes</Link>
+          <Link to="/mes-recettes" className="btn mobile-only">Carnet de Recettes</Link>
+
+          {/* Liens visibles seulement en mobile */}
+          <ul className="mobile-only-links">
+            <li><Link to="/rapides">Rapides</Link></li>
+            <li><Link to="/entrees">Entrées</Link></li>
+            <li><Link to="/plats">Plats</Link></li>
+            <li><Link to="/desserts">Desserts</Link></li>
+            <li><Link to="/aperitifs">Apéritifs</Link></li>
+          </ul>
+        </nav>
+
+        <div className="menu-toggle" onClick={toggleMenu}>
+          {isOpen ? <FaTimes /> : <FaBars />}
+        </div>
+      </div>
+    </header>
   )
 }
