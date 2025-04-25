b21c528430de449471dc5620e2e1d2b2315cfb32
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-04-25 17:21:58 +0100

    ✨  feat(F04): Home >  Ajout du composant <CurrentNews/>

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Category from './sections/Category'
 import '../../styles/home.css'
 import Banner from './sections/Banner'
+import CurrentNews from './sections/CurrentNews'
 
 export default function Home() {
   return (
     <div className='home'>
         <Banner/>
         <Category/>
+        <CurrentNews/>
     </div>
   )
 }
