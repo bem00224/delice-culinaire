3294e1fa7e3bd65e34c814168974c29b2e6132de
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-04-25 19:07:52 +0100

    ✨  feat(F04): refactorisation du composant Menu avec affichage dynamique des données via Cards

--- src/components/pages/Menu.js
+++ src/components/pages/Menu.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import Cards from '../reusable-ui/Cards'
+import '../../styles/menu.css'
 
-export default function Menu() {
+export default function Menu({ title,data , basePath=''}) {
   return (
-    <div>Menu</div>
+    <div className='menu'>
+        <div className='menu-title'>
+            <h3 className="menu-title-global">{title}</h3>
+        </div>
+        <div className='menu-container'>
+            <div className='menu-container-detail'>
+                {data.map(({imageSource, title, slug}) => (
+                    <Cards 
+                        key={slug}
+                        to={`${basePath}/${slug}`}
+                        title={title}
+                        image={imageSource}
+                    />
+                ) )}
+            </div>
+        </div>
+    </div>
   )
 }
