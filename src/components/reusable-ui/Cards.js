2d2449c5f0c4c631e4ce1010fc1b37fab686e321
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-04-25 19:06:18 +0100

    ✨  feat(F04):: création du composant Cards avec props dynamiques (image, titre, lien)

--- src/components/reusable-ui/Cards.js
+++ src/components/reusable-ui/Cards.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import '../../styles/cards.css'
 
-export default function Cards() {
+export default function Cards({to, image, title}) {
   return (
-    <div>Cards</div>
+    <>
+        <Link to={to} className='card-link'>
+            <div className='card-container'>
+                {image && <img src={image} alt={title} className='card-img' />}
+                <h3 className='card-title'>{title}</h3>
+            </div>
+        </Link>
+    </>
   )
 }
