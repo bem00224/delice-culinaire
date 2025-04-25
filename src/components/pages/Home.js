3508ecba0bb48db7102d9f45c90815709e8afb44
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-04-25 16:26:36 +0100

    ✨  feat(F04): Home >  Ajout du composant <Banner/>

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Category from './sections/Category'
 import '../../styles/home.css'
+import Banner from './sections/Banner'
 
 export default function Home() {
   return (
     <div className='home'>
+        <Banner/>
         <Category/>
     </div>
   )
