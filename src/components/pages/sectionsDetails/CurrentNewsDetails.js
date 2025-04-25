bbdfbd572f2978c1636d5b8bb519bd521f369d3b
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-04-25 19:48:22 +0100

    ✨  feat(F04): ajout du rendu détaillé pour les actualités avec image, temps, ingrédients et instructions

--- src/components/pages/sectionsDetails/CurrentNewsDetails.js
+++ src/components/pages/sectionsDetails/CurrentNewsDetails.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { useParams } from 'react-router-dom';
 import { fakeData } from '../../../data/fakeData';
 import Title from '../../reusable-ui/Title';
+import Image from '../../reusable-ui/Image';
+import CarnetButton from '../../reusable-ui/CarnetButton';
+import Ingredients from '../../reusable-ui/Ingredients';
+import Instructions from '../../reusable-ui/Instructions';
 
 export default function CurrentNewsDetails() {
     const { slug } = useParams();
@@ -9,8 +13,18 @@ export default function CurrentNewsDetails() {
     const currentItem = fakeData.CurrentNews.find((item) => item.slug === decodeURIComponent(slug));
     if (!currentItem) { return <p className="not-found">Recette introuvable 🥲.</p>; }
   return (
-    <div className="currentnews-details">
+    <div className="category-details-recipe">
         <Title title={currentItem.title}/>
+        <div className='category-details-bottom'>
+            <Image imageSource={currentItem.imageSource} title={currentItem.title} preparationTime={currentItem.preparationTime} cookingTime={currentItem.cookingTime} />
+            <div className='category-details-bottom-description'>
+                <CarnetButton recipe={{...currentItem, basePath: 'current-news'}}/>
+                <div className='category-details-section'>
+                    <Ingredients recipe={currentItem}/>
+                    <Instructions recipe={currentItem}/>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
