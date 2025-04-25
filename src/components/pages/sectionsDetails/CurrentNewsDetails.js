e8206903e187867e22589220f0ca098094ff4113
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-04-25 19:10:17 +0100

    ✨  feat(F04): ajout de CurrentNewsDetails avec affichage dynamique via slug

--- src/components/pages/sectionsDetails/CurrentNewsDetails.js
+++ src/components/pages/sectionsDetails/CurrentNewsDetails.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
+import { fakeData } from '../../../data/fakeData';
+import Title from '../../reusable-ui/Title';
 
 export default function CurrentNewsDetails() {
+    const { slug } = useParams();
+    // Trouver l'élément correspondant
+    const currentItem = fakeData.CurrentNews.find((item) => item.slug === decodeURIComponent(slug));
+    if (!currentItem) { return <p className="not-found">Recette introuvable 🥲.</p>; }
   return (
-    <div>CurrentNewsDetails</div>
+    <div className="currentnews-details">
+        <Title title={currentItem.title}/>
+    </div>
   )
 }
