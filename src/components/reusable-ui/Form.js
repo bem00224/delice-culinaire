bd263d1da9067138b775d472791e007fa0c6490c
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-04-25 15:59:43 +0100

    ✨  feat(F04): Form >  Ajout de l'input et du layout

--- src/components/reusable-ui/Form.js
+++ src/components/reusable-ui/Form.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../../styles/form.css'
 
 export default function Form() {
+    const [searchValue, setSearchValue] = useState('');
   return (
-    <div>Form</div>
+    <form className='formulaire'>
+        <div className='formulaire-content'>
+            <input 
+                type='search' 
+                id='search_input' 
+                placeholder='Je cherche une recette, un ingrédient...' 
+                className='input-search' 
+                value={searchValue} 
+                onChange={(e) => setSearchValue(e.target.value)}
+            />
+        </div>
+        <div className='btn-search'></div>
+    </form>
   )
 }
