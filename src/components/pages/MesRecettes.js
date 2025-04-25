b8d9423cef29094c2da94e9037d8339f738773cb
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-04-25 11:22:12 +0100

    ✨ feat(F03): Ajout de l'affichage des recettes sauvegardées dans la page MesRecettes

--- src/components/pages/MesRecettes.js
+++ src/components/pages/MesRecettes.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+import '../../styles/mesRecettes.css'
 
 export default function MesRecettes() {
+    const [savedRecipes, setSavedRecipes] = useState([])
+
+    useEffect(() => {
+        const saved = JSON.parse(localStorage.getItem('savedRecipes')) || [];
+        setSavedRecipes(saved)
+      }, [])
   return (
-    <div className='recette-global'>MesRecettes</div>
+    <div className='recette-global'>
+        <h2>{savedRecipes.length === 0 ? "" : "Mes Recettes"}</h2>
+        <div className="recettes-container">
+            {
+                savedRecipes.length === 0 ? (
+                    <div className='recettes-container-detail fade-in-up'>
+                        <div className="recettes-container-top">
+                            <p>Vous n'avez aucune recette enregistrée.</p>
+                            <p>Naviguez à travers le site et enregistrez vos recettes préférées! 😊</p>
+                            <Link to={"/"}>Retourner sur la page d'accueil</Link>
+                        </div>
+                    </div>
+                ) : (
+                    savedRecipes.map((recipe) => (
+                        <div className="recette-card" key={recipe.id}>
+                            <div className='recette-card-description'>
+                                <Link to={`/${recipe.basePath}/${recipe.slug}`} className='description-link'>
+                                    <div className='recette-card-description-image'>
+                                        <img src={recipe.imageSource} alt={recipe.title} />
+                                    </div>
+                                </Link>
+                            </div>
+                        </div>
+                    ) )
+                )
+            }
+        </div>
+    </div>
   )
 }
