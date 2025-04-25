2c87299d7ea122f7fb867778045524d06a0f02dc
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-04-25 15:06:13 +0100

    ✨ feat(F03):  Ajout de la fonction de suppression d'une carte

--- src/components/pages/MesRecettes.js
+++ src/components/pages/MesRecettes.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import '../../styles/mesRecettes.css'
+import { RiDeleteBin6Line } from 'react-icons/ri';
+import { toast } from 'react-toastify';
 
 export default function MesRecettes() {
     const [savedRecipes, setSavedRecipes] = useState([])
@@ -9,6 +11,13 @@ export default function MesRecettes() {
         const saved = JSON.parse(localStorage.getItem('savedRecipes')) || [];
         setSavedRecipes(saved)
       }, [])
+
+      const handleDelete = (slug) => {
+        const filtered = savedRecipes.filter(recipe => recipe.slug !== slug)
+        localStorage.setItem('savedRecipes', JSON.stringify(filtered))
+        setSavedRecipes(filtered)
+        toast.success("Recette supprimée !")
+    }
   return (
     <div className='recette-global'>
         <h2>{savedRecipes.length === 0 ? "" : "Mes Recettes"}</h2>
@@ -24,15 +33,19 @@ export default function MesRecettes() {
                     </div>
                 ) : (
                     savedRecipes.map((recipe) => (
-                        <div className="recette-card" key={recipe.id}>
+                        <div className="recette-card" key={recipe.slug}>
                             <div className='recette-card-description'>
                                 <Link to={`/${recipe.basePath}/${recipe.slug}`} className='description-link'>
                                     <div className='recette-card-description-image'>
-                                        <img src={recipe.imageSource} alt={recipe.title} />
+                                        <img src={recipe.imageSource} alt={recipe.title} loading="lazy"/>
                                     </div>
                                 </Link>
                                 <div className='recette-card-detail'>
                                     <h3>{recipe.title}</h3>
+                                    <button className='recette-card-detail-text' onClick={() => handleDelete(recipe.slug)}>
+                                        <RiDeleteBin6Line className='details-link' />
+                                        <span >Supprimer</span>
+                                    </button>
                                 </div>
                             </div>
                         </div>
