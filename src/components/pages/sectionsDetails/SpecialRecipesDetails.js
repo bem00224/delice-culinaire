a7082bee908c58c97a5ef982e162c5fcacf1bc4b
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-04-26 22:35:12 +0100

    ✨  feat(F05): création du composant specialRecipes et Affichage des details de la carte à travers notre reusable-ui RecipeDetailsLayout

--- src/components/pages/sectionsDetails/SpecialRecipesDetails.js
+++ src/components/pages/sectionsDetails/SpecialRecipesDetails.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { fakeData } from '../../../data/fakeData';
+import { useParams } from 'react-router-dom';
+import RecipeDetailsLayout from '../../reusable-ui/RecipeDetailsLayout';
 
 export default function SpecialRecipesDetails() {
+    const { slug } = useParams();
+
+  // Trouver l'élément correspondant
+  const specialRecipes = fakeData.SpecialRecipes.find((item) => item.slug === decodeURIComponent(slug));
   return (
-    <div>SpecialRecipesDetails</div>
+    <RecipeDetailsLayout recipe={specialRecipes} basePath="special-recipes" />
   )
 }
