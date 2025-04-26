e2c3f4cb6cbc8a2cba977c2ed1262c7d75303582
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-04-26 18:29:24 +0100

    ✨  feat(F06): création du composant Recipes et Affichage du contenu Menu

--- src/components/pages/sections/SpecialRecipes.js
+++ src/components/pages/sections/SpecialRecipes.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import Menu from '../Menu'
+import { fakeData } from '../../../data/fakeData'
 
 export default function SpecialRecipes() {
   return (
-    <div>SpecialRecipes</div>
+    <Menu title="Recettes Anti-Gaspillage" data={fakeData.SpecialRecipes} basePath='special-recipes' />
   )
 }
