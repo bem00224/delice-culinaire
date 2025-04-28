d98223f21c0b0a652757eaa7566f8009a1311905
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-04-28 14:56:25 +0100

    ✨  feat(F06): navbar > amélioration de la Navbar avec icône, nouveau titre et catégories dynamiques

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,35 +2,45 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/navbar.css'
 import { FaBars, FaTimes } from "react-icons/fa";
-import logo from "../assets/logo.png"
+import { fakeData } from '../data/fakeData';
+import { FaKitchenSet } from "react-icons/fa6";
 
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false)
   const toggleMenu = () => setIsOpen(!isOpen);
+  let category = fakeData.Category;
+  const closeMenu = () => setIsOpen(false);
 
   return (
     <header className="navbar">
       <div className='navbar-container'>
         <Link to="/" className="navbar-logo">
-          <img src={logo} alt="logo" className="logo" loading="lazy"/>
+          <div className='navbar-logo-icon'>
+            <FaKitchenSet className="logo-icon"/>
+          </div>
+          <h1 className="title-logo">Délices Culinaires</h1>
         </Link>
         <nav className={`navbar-menu ${isOpen ? "open" : ""}`}>
           <Link to="/mes-recettes" className="btn desktop-only">Mes recettes</Link>
-          <Link to="/mes-recettes" className="btn mobile-only">Carnet de Recettes</Link>
+          <Link to="/mes-recettes" className="btn mobile-only" onClick={closeMenu}>Carnet de Recettes</Link>
 
           {/* Liens visibles seulement en mobile */}
           <ul className="mobile-only-links">
-            <li><Link to="/rapides">Rapides</Link></li>
-            <li><Link to="/entrees">Entrées</Link></li>
-            <li><Link to="/plats">Plats</Link></li>
-            <li><Link to="/desserts">Desserts</Link></li>
-            <li><Link to="/aperitifs">Apéritifs</Link></li>
+            {
+              category.map((cat) => (
+                <li key={cat.slug}>
+                  <Link to={`/category/${encodeURIComponent(cat.slug)}`} onClick={closeMenu}>
+                  {cat.slug}
+                  </Link>
+                </li>
+              ) )
+            }
           </ul>
         </nav>
 
         <div className="menu-toggle" onClick={toggleMenu}>
-          {isOpen ? <FaTimes /> : <FaBars />}
+          {isOpen ? <FaTimes className="menu-toggle-icon" /> : <FaBars className="menu-toggle-icon"/>}
         </div>
       </div>
     </header>
